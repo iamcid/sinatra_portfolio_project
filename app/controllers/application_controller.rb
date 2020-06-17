cf555a26b7e7dd60ff145ce0b46dd3b9c1f31cee
iamcid <iamcid17@gmail.com>
2020-06-17 19:44:28 -0400

    wires up user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,19 @@ class ApplicationController < Sinatra::Base
     erb :homepage
   end
 
+  helpers do
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+    
+    def authenticate
+      redirect '/login' if !logged_in?
+    end
+  end
+
 end
