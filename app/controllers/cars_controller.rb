61dd4e02a0ff6665cb97ec049851d7bce0b31e24
iamcid <iamcid17@gmail.com>
2020-06-21 23:37:58 -0400

    adds car to user's dashboard and creates edit link

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,7 +5,17 @@ class CarsController < ApplicationController
     end
 
     post '/cars' do
+        if !logged_in?
+            redirect '/'
+        end
 
+        @car = @user.cars.build(year: params[:year], make: params[:make], model: params[:model], color: params[:color])
+        
+        if @car.save
+            redirect "/users/dashboard"
+        else
+            redirect '/cars/new'
+        end
     end
 
 end
